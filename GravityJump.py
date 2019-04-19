f77a3f85e934c05fb2bf6131bc597a259973b1fc
Jeremy Skoler <jskoler@olin.edu>
2019-04-19 10:56:32 -0400

    Startign Map class implementation (incomplete)

--- GravityJump.py
+++ GravityJump.py
@@ -35,7 +35,8 @@ done = False
 clock = pg.time.Clock()
 
 #Keeps track of the platforms
-plat_list = []
+plat_rect_list = []
+plat_obj_list = []
 
 
 class Platform:
@@ -43,7 +44,7 @@ class Platform:
 
     """
     def __init__(self):
-        self.width = 200
+        self.width = 150
         self.height = 10
         self.plat = pg.Surface((self.width, self.height))
         self.plat.fill(LUSHGREEN)
@@ -54,11 +55,49 @@ class Platform:
         self.y = y
         screen.blit(self.plat,(self.x  - (self.width/2), self.y - (self.height/2)))
         if self.initialized == False:
-            plat_list.append(pg.Rect(self.x  - (self.width/2), self.y - (self.height/2), self.width, self.height))
+            plat_rect_list.append(pg.Rect(self.x  - (self.width/2), self.y - (self.height/2), self.width, self.height))
             self.initialized = True
+        for rect in plat_rect_list:
+            rect = pg.Rect(self.x  - (self.width/2), self.y - (self.height/2), self.width, self.height)
 
-    #def generate(self):
+class Map:
+    """Generates Map
 
+    """
+    def __init__(self):
+        self.spacing = 30
+        self.counter = 0
+
+    def initialize(self):
+        for num in range(100,900,100):
+            self.generate_plat(num)
+
+
+    def proximity_check(self):
+        if plat_obj_list[-1].top < self.spacing:
+            return False
+        else:
+            return True
+
+    def off_the_edge(self):
+        if plat_obj_list[0].top > screen_height:
+            plat_obj_list.remove(plat_obj_list[0])
+
+
+    def generate_plat(self, y):
+            plat = Platform()
+            plat_obj_list.append(plat)
+            plat.draw_platform(random.randint(0,screen_width),y)
+
+    def move_map(self):
+        if pg.key.get_pressed()[pg.K_DOWN]:
+            for plat in plat_obj_list:
+                plat.y += 5
+
+    def run_map(self):
+        self.move_map()
+        self.generate_plat()
+        self.off_the_edge()
 
 
 class Character:
@@ -75,11 +114,10 @@ class Character:
         self.falling = False
 
 
-
     def draw_sqr(self):
         self.bottom_collider = pg.Rect(self.x-(self.size/2), self.y + (self.size/2), self.size, 5)
         if self.collision_detection() and self.delta_y > 0:
-            self.y = (plat_list[self.index].top) - (self.size/2)
+            self.y = (plat_rect_list[self.index].top) - (self.size/2)
             self.v = -60
         self.surf = pg.Surface((self.size,self.size))
         self.surf.fill(RED)
@@ -101,11 +139,13 @@ class Character:
         if self.x < 0:
             self.x = screen_width
         #Checks if characters collider is in collision with a platform
-        if self.bottom_collider.collidelist(plat_list) != -1:
-            self.index = self.bottom_collider.collidelist(plat_list)
+
+        if self.bottom_collider.collidelist(plat_rect_list) != -1:
+            self.index = self.bottom_collider.collidelist(plat_rect_list)
             return True
         else:
             return False
+
 
     def move(self):
         self.jump()
@@ -114,8 +154,10 @@ class Character:
         if pg.key.get_pressed()[pg.K_RIGHT]:
             self.x += 4
 
+
     def update_state(self):
         self.bottom_collider = pg.Rect(self.x-20, self.y + 18, 40, 5)
+
 
     def create_character(self):
         self.draw_sqr()
@@ -123,13 +165,11 @@ class Character:
         #self.update_state()
 
 
-
+map = Map()
 first_plat = Platform()
-plat2= Platform()
-plat3 = Platform()
-plat4= Platform()
-plat5 = Platform()
+map.initialize()
 red = Character()
+print(plat_rect_list)
 # -------- Main Program Loop -----------
 while not done:
     for event in pg.event.get():
@@ -143,16 +183,14 @@ while not done:
     # First, clear the screen to WHITE. Don't put other drawing commands
     # above this, or they will be erased with this command.
     screen.fill(WHITE)
-
+    for plat in plat_obj_list:
+        plat.draw_platform(plat.x,plat.y)
     # Limit frames per second
     tick = clock.tick(60)
 
-
+    map.run_map()
+    #map.initialize()
     first_plat.draw_platform(350, 650)
-    plat2.draw_platform(450, 500)
-    plat3.draw_platform(50,100)
-    plat4.draw_platform(50,400)
-    plat5.draw_platform(200, 300)
     red.create_character()
 
     #Update the screen
