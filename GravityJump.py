fd84dea53262c842a6be824b0b577445184eeb4f
Jeremy Skoler <jskoler@olin.edu>
2019-04-29 20:31:34 -0400

    dissapearing platforms and counters

--- GravityJump.py
+++ GravityJump.py
@@ -33,6 +33,7 @@ done = False
 # Used to manage how fast the screen updates
 clock = pg.time.Clock()
 
+font = pg.font.SysFont('chandas', 20, True)
 
 class Platform:
     """Create a new platform
@@ -40,7 +41,7 @@ class Platform:
     """
     #Initialize platform attributes
     def __init__(self):
-        self.width = 150
+        self.width = 120
         self.height = 10
         #This will later be changed to a more visually appealing platform
         self.plat = pg.Surface((self.width, self.height))
@@ -64,7 +65,6 @@ class Platform:
         self.top = self.rect.top
         screen.blit(self.plat,(self.x  - (self.width/2), self.y - (self.height/2)))
 
-
 class Character():
     """Build character
 
@@ -75,9 +75,12 @@ class Character():
     Currently the characters are represented as square surfaces. This will be changed to animated sprites.
     """
 
-    def __init__(self, color, left, right, x = 150, y = 825):
+    def __init__(self,name, color, left, right, fire, x = 150, y = 825, score=0):
+        self.name = name
+        self.color = color
         self.left = left
         self.right = right
+        self.fire = fire
         self.x = x
         self.y = y
         self.size = 40
@@ -85,7 +88,11 @@ class Character():
         self.g = 9.81
         self.delta_y = 0
         self.surf = pg.Surface((self.size,self.size))
-        self.surf.fill(color)
+        self.surf.fill(self.color)
+        self.score = score
+        self.babykangas = []
+        self.pouch = 2
+        self.lives = 3
 
 
     def draw_character(self):
@@ -107,18 +114,27 @@ class Character():
         self.jump()
         #Left,Right controls
         if pg.key.get_pressed()[self.left]:
-            self.x += -4
+            self.x += -5
         if pg.key.get_pressed()[self.right]:
-            self.x += 4
+            self.x += 5
+        """if pg.key.get_pressed()[self.fire] and len(self.babykangas) == 0:
+            babykanga = BabyKanga(self.x,self.y)
+            self.babykangas.append(babykanga)
+            self.pouch -= 1"""
 
-    def boundry_detection(self):
+
+    def boundry_detection(self,other):
         """ Detect screen boundries and adjust accordingly.
 
         If the character falls off the bottom they reappear at the top. If they go
         off a side they loop to the other side.
         """
-        if self.y > screen_height:
-            self.y = 400
+        if self.y >= screen_height:
+            self.y = 825
+            #self.end_screen(other)
+            other.score += 1
+            map.reset()
+
         if self.x > screen_width:
             self.x = 0
         if self.x < 0:
@@ -156,6 +172,22 @@ class Character():
             self.y = (rect_list[self.current_plat].top) - (self.size/2)
             #Starts a new jump
             self.v = -60
+            #initiates falling function
+            obj_list.remove(obj_list[self.current_plat])
+
+    def pouch_detection(self, other):
+        location_rect = self.surf.get_rect(center=(self.x,self.y))
+        if location_rect.collidelist(other.babykangas):
+            self.pouch +=  1
+            self.lives -= 1
+            if self.lives == 0:
+                other.score += 1
+                map.reset()
+
+    def idk(self):
+        if self.babykangas[0].y <= 0:
+            self.babykangas.remove(self.babykangas[0])
+
 
     def run_character(self,other):
         """Run character functions
@@ -164,9 +196,45 @@ class Character():
         """
         self.draw_character()
         self.move()
-        self.boundry_detection()
+        self.boundry_detection(other)
         self.scroll_detection(other)
+        #self.pouch_detection(other)
         self.collision_detection(map.plat_obj)
+
+class BabyKanga:
+
+        #Initialize babykanga instance.(Tank instance firing, opponent's Tank instance)
+    def __init__(self, kangx,kangy, name='joey'):
+        self.x = kangx
+        self.y = kangy
+        self.mother = name
+        self.vert_speed = 9
+        self.v = -60
+        self.g = 9.81
+        self.delta_y = 0
+        self.where = True
+
+        #Draws babykanga
+    def draw_babykanga(self):
+        pg.draw.rect(screen, DRIEDBLOOD, (self.x, self.y, 4, 8), 0)
+        self.trajectory2()
+        self.rect = pg.Rect(self.x, self.y, 4, 4)
+
+    def trajectory2(self):
+        self.y -= 8
+
+    def trajectory1(self):
+        if self.where == True and self.y >= 250:
+            self.y -= self.vert_speed
+        else:
+            #change sprite
+            time = tick/40
+            self.delta_y = self.v * time + 0.5 * self.g * math.pow(time, 2)
+            self.v = self.v + time * self.g
+            self.y += self.delta_y
+            self.where = False
+            if self.delta_y >= 0:
+                self.x = self.mother.x
 
 
 class Map:
@@ -189,8 +257,10 @@ class Map:
         """
         self.generate_plat(150, 850)
         self.generate_plat(450, 850)
-        for num in range(50,850,50):
+        for num in range(100,900,50):
             self.generate_plat(random.randint(0 ,screen_width), 900 - num)
+            if random.randint(0,4) == 4:
+                self.generate_plat(random.randint(0 ,screen_width), 900 - num)
 
     def generate_plat(self, x, y):
         """Create new platform object at specified Y coordinate
@@ -235,19 +305,45 @@ class Map:
     def new_plat(self):
         if self.proximity_check():
             self.generate_plat(random.randint(0, screen_width),-5)
+            if random.randint(1,3) == 1:
+                self.generate_plat(random.randint(0,screen_width),-5)
+
+    def score_board(self):
+        red_text = '%s has %d points!' % (red.name, red.score)
+        red_score = font.render(red_text, False, red.color, GREY)
+        screen.blit(red_score,(360,20))
+        blue_text = '%s has %d points!' % (blue.name, blue.score)
+        blue_score = font.render(blue_text, False, blue.color, GREY)
+        screen.blit(blue_score,(30,20))
+
+        """red_lives = '%s lives: %d' % (red.name, red.lives)
+        red_life_text = font.render(red_lives, False, red.color, GREY)
+        screen.blit(red_life_text,(360,50))
+        blue_lives = '%s lives: %d' % (blue.name, blue.lives)
+        blue_life_text = font.render(blue_lives, False, blue.color, GREY)
+        screen.blit(blue_life_text,(30,50))"""
+
+
+    def reset(self):
+        self.__init__()
+        self.initialize()
+        red.__init__('Red',RED, pg.K_LEFT, pg.K_RIGHT, pg.K_UP,450, score = red.score)
+        blue.__init__('Blue', BLUE, pg.K_a, pg.K_d, pg.K_w, score = blue.score)
 
     def run_map(self):
         """Run it all"""
         self.draw_map()
         self.off_the_edge()
         self.new_plat()
+        self.score_board()
 
 
-#Create and initialize class objects
 map = Map()
 map.initialize()
-red = Character(RED, pg.K_LEFT, pg.K_RIGHT)
-blue = Character(BLUE, pg.K_a, pg.K_d, 450)
+red = Character('Red',RED, pg.K_LEFT, pg.K_RIGHT, pg.K_UP, 450)
+blue = Character('Blue', BLUE, pg.K_a, pg.K_d, pg.K_w)
+
+
 
 # -------- Main Program Loop -----------
 while not done:
@@ -266,6 +362,15 @@ while not done:
 
     # Limit frames per second
     tick = clock.tick(60)
+
+
+    for babykanga in red.babykangas:
+        babykanga.draw_babykanga()
+
+    for babykanga in blue.babykangas:
+        babykanga.draw_babykanga()
+
+
 
     map.run_map()
     blue.run_character(red)
